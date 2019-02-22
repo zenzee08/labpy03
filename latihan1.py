b5711dc145d0c57f50b07dffeaf1f3757503861f
zaenal abidin <you@example.com>
2019-02-22 21:15:02 +0700

    tugas labpy03

---
+++ latihan1.py
@@ -0,0 +1,12 @@
+jumlah = int (input("Masukan Nilai N : "))
+import random
+jumlah = 5
+a = 0
+for x in range (jumlah):
+    i = random.uniform(0.0,0.5)
+    a+=1
+    print ('Data ke :',a,'==>', i)
+
+
+
+print ("^^^^Selesai^^^^^")
